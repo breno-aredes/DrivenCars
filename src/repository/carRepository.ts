c173532d79ec9624b61369db2113501f15791ceb
breno-aredes <breno_mg10@hotmail.com>
2023-04-13 16:48:34 -0300

    feat: code refactoring for prism to work

---
+++ src/repository/carRepository.ts
@@ -0,0 +1,59 @@
+import db from "../config/database.js";
+
+async function getCars() {
+  const data = await db.cars.findMany();
+  return data;
+}
+
+async function getCar(id: number) {
+  const data = await db.cars.findUnique({
+    where: {
+      id: id,
+    },
+  });
+
+  return data;
+}
+
+async function getCarWithLicensePlate(licensePlate: string) {
+  const data = await db.cars.findUnique({
+    where: {
+      licensePlate: licensePlate,
+    },
+  });
+  return data;
+}
+
+async function createCar(
+  model: string,
+  licensePlate: string,
+  year: number,
+  color: string
+) {
+  await db.cars.create({
+    data: {
+      model,
+      licensePlate,
+      year,
+      color,
+    },
+  });
+}
+
+async function deleteCar(id: number) {
+  await db.cars.delete({
+    where: {
+      id,
+    },
+  });
+}
+
+const carRepository = {
+  getCar,
+  getCarWithLicensePlate,
+  getCars,
+  createCar,
+  deleteCar,
+};
+
+export default carRepository;
